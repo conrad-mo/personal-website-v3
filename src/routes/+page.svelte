5e6198e366818603421fd459ca332ebb43fd709c
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2025-11-15 16:20:18 -0500

    Add some stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,32 @@
-<div class="mx-64 flex min-h-screen flex-col justify-center gap-8">
-	<div class="flex flex-col gap-4 text-2xl text-gray-400">
-		<h1
+<div class="mx-64 my-16 flex min-h-screen flex-col gap-4">
+	<div class="flex flex-col gap-2 text-2xl text-gray-400">
+		<h1 class="text-4xl font-bold">
+			<span>👋</span>
+			<span class="bg-linear-to-r from-[#FFD3A5] to-[#FFB6B0] bg-clip-text text-transparent">
+				Hi! I'm Conrad Mo
+			</span>
+		</h1>
+		<!-- <h1
 			class="bg-linear-to-r from-[#FFD3A5] to-[#FD6585] bg-clip-text text-5xl font-bold text-transparent"
 		>
 			👋 Hi! I'm Conrad Mo
-		</h1>
+		</h1> -->
 		Computer Science undergrad at the University of Toronto
+	</div>
+	<div class="text-gray-400">
+		<h2 class="text-2xl font-medium text-gray-200">About me</h2>
+		<li>
+			<ul>Eating</ul>
+			<ul>Piano</ul>
+			<ul>Building</ul>
+		</li>
+	</div>
+	<div class="text-gray-400">
+		<h2 class="text-2xl font-medium text-gray-200">Where I've been</h2>
+		<li>
+			<ul>Shopify</ul>
+			<ul>Hack the Valley</ul>
+			<ul>Northern Labs</ul>
+		</li>
 	</div>
 </div>
