2a9b23aad2a4f1964d4794e4beba4a62e9290907
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2025-11-15 15:20:46 -0500

    Update deps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="mx-64 flex min-h-screen flex-col justify-center gap-8">
+	<div class="flex flex-col gap-4 text-2xl text-gray-400">
+		<h1
+			class="bg-linear-to-r from-[#FFD3A5] to-[#FD6585] bg-clip-text text-5xl font-bold text-transparent"
+		>
+			👋 Hi! I'm Conrad Mo
+		</h1>
+		Computer Science undergrad at the University of Toronto
+	</div>
+</div>
