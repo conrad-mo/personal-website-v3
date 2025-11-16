6a6ab46dca02eccfd3b18fc0cfa27dd775b19add
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2025-11-15 22:52:35 -0500

    Reduce animation

--- src/lib/components/ListItem.svelte
+++ src/lib/components/ListItem.svelte
@@ -15,7 +15,7 @@
 			<ArrowUpRight
 				size={16}
 				color="#FFB6B0"
-				class="transition-transform duration-300 group-hover:translate-x-1 group-hover:-translate-y-1"
+				class="transition-transform duration-300 group-hover:translate-x-0.5 group-hover:-translate-y-0.5"
 			/>
 			<span
 				class="absolute bottom-0 left-0 h-0.5 w-0 bg-[#FFB6B0] transition-all duration-300 group-hover:w-full"
