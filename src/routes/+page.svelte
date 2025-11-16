09557f5a6ab04f586bb5f3d6488ca26879a7df80
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2025-11-15 22:55:50 -0500

    Better contrast for subtitle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,7 @@
 
 <div class=" flex max-h-screen min-h-screen flex-col items-center justify-center">
 	<div class="flex flex-col gap-8">
-		<div class="flex flex-col gap-2 text-2xl text-gray-400">
+		<div class="flex flex-col gap-2 text-2xl text-gray-200">
 			<h1 class="text-4xl font-bold">
 				<span class="bg-linear-to-r from-[#FFD3A5] to-[#FFB6B0] bg-clip-text text-transparent">
 					> Hi! I'm Conrad Mo
