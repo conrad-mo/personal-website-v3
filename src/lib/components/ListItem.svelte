5f543b7fe284dca2cce6476793dec8442ee05ad9
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2025-11-16 13:22:12 -0500

    Underline text on hover only

--- src/lib/components/ListItem.svelte
+++ src/lib/components/ListItem.svelte
@@ -5,21 +5,19 @@
 
 <li>
 	{#if url}
-		<a
-			href={url}
-			class="group relative inline-flex items-center wrap-break-word"
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			<span class="break-all">- {text}</span>
-			<ArrowUpRight
-				size={16}
-				color="#FFB6B0"
-				class="ml-0.5 shrink-0 transition-transform duration-300 group-hover:translate-x-0.5 group-hover:-translate-y-0.5"
-			/>
-			<span
-				class="absolute bottom-0 left-0 h-0.5 w-0 bg-[#FFB6B0] transition-all duration-300 group-hover:w-full"
-			></span>
+		<a href={url} class="inline-flex items-center" target="_blank" rel="noopener noreferrer">
+			<span class="break-all">-&nbsp;</span>
+			<span class="group relative inline-flex items-center wrap-break-word">
+				<span class="break-all">{text}</span>
+				<ArrowUpRight
+					size={16}
+					color="#FFB6B0"
+					class="ml-0.5 shrink-0 transition-transform duration-300 group-hover:translate-x-0.5 group-hover:-translate-y-0.5"
+				/>
+				<span
+					class="absolute bottom-0 left-0 h-0.5 w-0 bg-[#FFB6B0] transition-all duration-300 group-hover:w-full"
+				></span>
+			</span>
 		</a>
 	{:else}
 		<span class="group relative inline-flex items-center">
