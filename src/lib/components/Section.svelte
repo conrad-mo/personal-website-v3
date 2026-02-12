bf31f21da040281b8aa4048f49e3b99f2478f78d
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2026-02-12 12:05:55 -0500

    Fix font size

--- src/lib/components/Section.svelte
+++ src/lib/components/Section.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex flex-col gap-2 text-gray-400">
-	<h2 class="text-xl font-medium text-gray-200 sm:text-2xl">{title}</h2>
+	<h2 class="text-xl font-medium text-gray-200">{title}</h2>
 	<ul class="flex flex-col gap-1.5 text-sm sm:gap-2 sm:text-base">
 		{@render children()}
 	</ul>
