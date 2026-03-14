116f2bafe3a5b4ff0be50ca6f16c05932f275440
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2026-03-14 18:31:00 -0400

    Test font preloads

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	import inter300 from '$lib/assets/fonts/inter-latin-300-normal.woff2?url';
-	import inter400 from '$lib/assets/fonts/inter-latin-400-normal.woff2?url';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
-	<link rel="preload" href={inter300} as="font" type="font/woff2" crossorigin="anonymous" />
-	<link rel="preload" href={inter400} as="font" type="font/woff2" crossorigin="anonymous" />
 </svelte:head>
 
 {@render children?.()}
